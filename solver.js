3e9bdb2409d18ff79616e3295af6423d1a172541
rhnvrm <rohanverma2004@gmail.com>
2015-09-26 23:08:03 +0530

    init

---
+++ solver.js
@@ -0,0 +1,40 @@
+var a = $('.challenge ').not('.finished').children('.move');
+var start = parseInt($('.challenge ').not('.finished').children('.start').children('.text')[0].innerHTML)
+for(i = 0; i < a.length; i++){
+  input = /<span .*>(.*)?<\/span>/g.exec(a[i].innerHTML)[1];
+
+  if(input === "multiply it by itself"){
+  	start *= start;
+  }
+
+  else if(input === "halve it"){
+  	start /= 2;
+  }
+
+  else if(input === "double it"){
+  	start *= 2;
+  }
+
+  else{
+  	num = input.substring(1,input.length)
+  	if(input[0] === "+"){
+  		start += parseInt(num);
+  		//console.log("+");
+  	}
+  	else if(input[0] === "-"){
+  		start -= parseInt(num);
+  		//console.log("-");
+  	}
+  	else if(input[0] === "÷"){
+  		start /= parseInt(num);
+  		//console.log("/");
+  	}  	
+  	else if(input[0] === "×"){
+  		start *= parseInt(num);
+  		//console.log("*");
+  	}  	
+  }
+  //console.log(start);
+}
+
+console.log(start);
