d5737dd0f9ae3a6f8398f89f1ed5526956ed8c94
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-26 18:26:04 +0530

    Medium game solver added

--- solver.js
+++ solver.js
@@ -1,47 +1,37 @@
-var superscripts = "⁰¹²³⁴⁵⁶⁷⁸⁹";
-var subscripts = "₀₁₂₃₄₅₆₇₈₉";
-
-
 var a = $('.challenge ').not('.finished').children('.move');
 var start = parseInt($('.challenge ').not('.finished').children('.start').children('.text')[0].innerHTML)
-for(i = 0; i < a.length; i++){
-  input = /<span .*>(.*)?<\/span>/g.exec(a[i].innerHTML)[1];
-
+for(var i = 0; i < a.length; i++){
+  var input = /<span .*>(.*)?<\/span>/g.exec(a[i].innerHTML)[1];
   if(input === "multiply it by itself"){
   	start *= start;
   }
-
   else if(input === "halve it"){
   	start /= 2;
   }
-
   else if(input === "double it"){
   	start *= 2;
   }
-
+  else if(input.search("of this") != -1){
+    var input = a[i].getElementsByClassName("text")[0].getAttribute("aria-label");
+    var num = parseInt(input[0]);
+    var den = parseInt(input[4]);
+    start = start * num;
+    start = start / den;
+  }
   else{
   	num = input.substring(1,input.length)
   	if(input[0] === "+"){
   		start += parseInt(num);
-  		//console.log("+");
   	}
   	else if(input[0] === "-"){
   		start -= parseInt(num);
-  		//console.log("-");
   	}
   	else if(input[0] === "÷"){
   		start /= parseInt(num);
-  		//console.log("/");
   	}  	
   	else if(input[0] === "×"){
   		start *= parseInt(num);
-  		//console.log("*");
   	}  	
   }
-  //console.log(start);
 }
-
-console.log(start);
-
 game.current_challenge.check(start)
-
