6bb9d5d0a8f9757664715c2d17b6e62d46420f36
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-27 13:23:16 +0530

    Hard solver added. Automated the solver too.

--- solver.js
+++ solver.js
@@ -3,35 +3,45 @@ var start = parseInt($('.challenge ').not('.finished').children('.start').childr
 for(var i = 0; i < a.length; i++){
   var input = /<span .*>(.*)?<\/span>/g.exec(a[i].innerHTML)[1];
   if(input === "multiply it by itself"){
-  	start *= start;
+    start *= start;
+  }
+  else if(input === "multiply it by itself twice"){
+    start = start*start*start;
   }
   else if(input === "halve it"){
-  	start /= 2;
+    start /= 2;
   }
   else if(input === "double it"){
-  	start *= 2;
+    start *= 2;
   }
   else if(input.search("of this") != -1){
-    var input = a[i].getElementsByClassName("text")[0].getAttribute("aria-label");
-    var num = parseInt(input[0]);
-    var den = parseInt(input[4]);
-    start = start * num;
-    start = start / den;
+    if(input.search("%") != -1){
+      var num = parseInt(input.substring(0,input.search("%")));
+      start = start * num;
+      start = start / 100;
+    }
+    else{
+      var input = a[i].getElementsByClassName("text")[0].getAttribute("aria-label");
+      var num = parseInt(input[0]);
+      var den = parseInt(input[4]);
+      start = start * num;
+      start = start / den;
+    }
   }
   else{
-  	num = input.substring(1,input.length)
-  	if(input[0] === "+"){
-  		start += parseInt(num);
-  	}
-  	else if(input[0] === "-"){
-  		start -= parseInt(num);
-  	}
-  	else if(input[0] === "÷"){
-  		start /= parseInt(num);
-  	}  	
-  	else if(input[0] === "×"){
-  		start *= parseInt(num);
-  	}  	
+    var num = input.substring(1,input.length);
+    if(input[0] === "+"){
+      start += parseInt(num);
+    }
+    else if(input[0] === "-"){
+      start -= parseInt(num);
+    }
+    else if(input[0] === "÷"){
+      start /= parseInt(num);
+    }   
+    else if(input[0] === "×"){
+      start *= parseInt(num);
+    }   
   }
 }
 game.current_challenge.check(start)
